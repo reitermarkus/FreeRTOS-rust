59d934bc9faf78d7d5fdb48edea39d5df5738107
Markus Reiter <me@reitermark.us>
2022-07-29 20:24:53 +0200

    Improve and simplify build script.

--- freertos-rust/build.rs
+++ freertos-rust/build.rs
@@ -1,5 +1,6 @@
 use std::env;
 use std::path::PathBuf;
+use std::process::exit;
 
 // See: https://doc.rust-lang.org/cargo/reference/build-scripts.html
 fn main() {
@@ -12,34 +13,40 @@ fn main() {
     println!("cargo:SHIM={}", shim_dir.display());
 
     if let Ok(freertos_source) = env::var("FREERTOS_SRC") {
-      let heap = if env::var("CARGO_FEATURE_HEAP_4").is_ok() {
-        Some("heap_4.c")
-      } else if env::var("CARGO_FEATURE_HEAP_3").is_ok() {
-        Some("heap_3.c")
-      } else if env::var("CARGO_FEATURE_HEAP_2").is_ok() {
-        Some("heap_2.c")
-      } else if  env::var("CARGO_FEATURE_HEAP_1").is_ok() {
-        Some("heap_1.c")
-      } else {
-        None
-      };
+      let mut heap = None;
+      for i in 1..=5 {
+        if env::var(&format!("CARGO_FEATURE_HEAP_{i}")).is_ok() {
+          if let Some(h) = heap {
+            eprintln!("Features `heap_{h}` and `heap_{i}` are mutually exclusive.");
+            exit(1);
+          }
+
+          heap = Some(i);
+        }
+      }
+      let heap = format!("heap_{}.c", heap.unwrap_or(4));
 
       let mut freertos_builder = freertos_cargo_build::Builder::new();
-      let pwd = PathBuf::from(env::var("PWD").unwrap());
-      let freertos_source = pwd.join(freertos_source);
-      let freertos_config = pwd.join(env::var("FREERTOS_CONFIG").unwrap());
+      let freertos_source = PathBuf::from(freertos_source);
+      let freertos_config = match env::var("FREERTOS_CONFIG") {
+        Ok(path) => PathBuf::from(path),
+        Err(_) => {
+          eprintln!("`FREERTOS_CONFIG` must be set to the directory containing `FreeRTOSConfig.h`.");
+          exit(1);
+        }
+      };
 
       freertos_builder.get_cc().define("RUST", None);
 
       freertos_builder.freertos(&freertos_source);
       freertos_builder.freertos_config(&freertos_config);
       freertos_builder.freertos_shim(&shim_dir);
+      freertos_builder.heap(heap);
 
-      if let Some(heap) = heap {
-        freertos_builder.heap(heap.to_owned());
+      if let Err(err) = freertos_builder.compile() {
+        eprintln!("Compilation failed: {}", err);
+        exit(1);
       }
-
-      freertos_builder.compile().unwrap_or_else(|e| { panic!("{}", e.to_string()) });
 
       bindgen::builder()
           .use_core()
@@ -48,7 +55,13 @@ fn main() {
           .clang_arg(format!("-I{}", freertos_config.display()))
           .clang_arg(format!("-I{}", freertos_builder.get_freertos_port_dir().display()))
           .header(shim_dir.join("shim.c").display().to_string())
-          .generate().unwrap()
-          .write_to_file(out_dir.join("shim.rs")).unwrap();
+          .generate().unwrap_or_else(|err| {
+            eprintln!("Failed generating bindings: {}", err);
+            exit(1);
+          })
+          .write_to_file(out_dir.join("shim.rs")).unwrap_or_else(|err| {
+            eprintln!("Failed writing bindings: {}", err);
+            exit(1);
+          });
     }
 }
