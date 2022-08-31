b4cc8114b36b0629c2a4f627f9cfa4ddb0f54406
Markus Reiter <me@reitermark.us>
2022-08-31 10:57:08 +0200

    Simplify allocator.

--- freertos-rust/src/allocator.rs
+++ freertos-rust/src/allocator.rs
@@ -13,11 +13,10 @@ pub struct FreeRtosAllocator;
 
 unsafe impl GlobalAlloc for FreeRtosAllocator {
     unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
-        let res = pvPortMalloc(layout.size() as u32);
-        return res as *mut u8;
+        pvPortMalloc(layout.size() as _).cast()
     }
 
     unsafe fn dealloc(&self, ptr: *mut u8, _layout: Layout) {
-      vPortFree(ptr as FreeRtosVoidPtr)
+        vPortFree(ptr.cast())
     }
 }
